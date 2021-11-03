2676dde966e20a950da7f69b7c87764bab998579
arfian-rp <arfianrafi77@gmail.com>
2021-11-03 22:59:15 +0700

    ok

---
+++ dists/precache-manifest.b44e029f94c0b1949dd170ae066529b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b58f963e7190f24d9b1a",
+    "url": "/css/app.0c87714b.css"
+  },
+  {
+    "revision": "6fe93fdc23765917369df6ecf1c78991",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b58f963e7190f24d9b1a",
+    "url": "/js/app.04b42a27.js"
+  },
+  {
+    "revision": "6f5996c1114d3ae3ddab",
+    "url": "/js/chunk-vendors.d5ce3bf1.js"
+  },
+  {
+    "revision": "780f836e6b3de18110f876e8b7a0df45",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
